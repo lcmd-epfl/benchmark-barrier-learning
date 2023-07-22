1e33cc5718bb5142d0e8a4ebc007f4cc457344bd
Ksenia <ksenia.briling@epfl.ch>
2023-07-22 22:35:04 +0200

    Generate reactants and products SMILES

--- src/proparg2smiles_new.py
+++ src/proparg2smiles_new.py
@@ -3,6 +3,10 @@
 from rdkit import Chem
 
 
+def clean_smiles(x):
+    return Chem.MolToSmiles(Chem.MolFromSmiles(x))
+
+
 def construct_ligands():
 
     ''' Labeled Hydrodens are fragment linkers.
@@ -31,8 +35,8 @@ def construct_ligands():
         'j' : 'c1([H:1004])ccccc1'   ,  # Ph
     }
 
-    def concatenate_fragments(a, b, nH, nbond=9):
-        return (a + '.' + b).replace(f'([H:{nH}])', f'%{nbond+nH-1000+10}')
+    def concatenate_fragments(a, b, nH):
+        return (a+'.'+b).replace(f'([H:{nH}])', f'%{nH-1000+20}')
 
     scaffolds = {}
     scaffolds[1, 'bp'] = '[H]c1c([H:1002])c([H:1001])[n+]([O-])c([H:1004])c1([H:1003])'
@@ -51,16 +55,47 @@ def construct_ligands():
         for X, X_frag in X_frags.items():
             tmp = concatenate_fragments(scaffold, X_frag, 1004)
             tmp = concatenate_fragments(tmp, tmp, 1001)
-            tmp = Chem.MolToSmiles(Chem.MolFromSmiles(tmp))
+            tmp = clean_smiles(tmp)
             ligands[f'{Y}{X}{R}'] = tmp
 
     return ligands
 
 
+def construct_reactants_products(ligands):
+
+    products = {}
+    reactants = {}
+
+    for key, lig in ligands.items():
+
+        lig = lig.replace('[O-]', 'O%31', 1).replace('[O-]', 'O%32', 1)
+
+        center_product = '[Si+](Cl)(Cl)%31%32%33'
+        product = 'C(#CCC(C1=CC=CC=C1)O%33)[H]'
+        complex_product = lig+'.'+center_product+'.'+product
+        complex_product = clean_smiles(complex_product)
+        products[key] = complex_product
+
+        center_reactant = '[Si](Cl)(Cl)%31%32%33%34'
+        reactant1 = 'C=C=C%33'
+        reactant2 = 'C1(=CC=CC=C1)C=[O+]%34' if True else 'C1(=CC=CC=C1)[CH+][O]%34'   # TODO ask Simone
+        complex_reactant = lig+'.'+ center_reactant+'.'+reactant1+'.'+reactant2
+        complex_reactant = clean_smiles(complex_reactant)
+        reactants[key] = complex_reactant
+
+    return products, reactants
+
+
 def main(data_dir='benchmark-barrier-learning/data/proparg'):
+
     ligands = construct_ligands()
-    for i, j in ligands.items():
-        print(i, j)
+    products, reactants = construct_reactants_products(ligands)
+
+    for key in ligands.keys():
+        print(key)
+        print(products[key])
+        print(reactants[key])
+        print()
 
 
 if __name__=='__main__':

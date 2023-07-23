1c95e34e171bba4a1e33a417e5737146bec036bc
Ksenia <ksenia.briling@epfl.ch>
2023-07-23 05:27:22 +0200

    Get almost correct atom-mapped smiles for proparg
    1) not sure which resonance structure of reactant2
       in construct_reactants_products() is better
    2) no idea what to do with chirality

--- src/proparg2smiles_new.py
+++ src/proparg2smiles_new.py
@@ -1,7 +1,81 @@
 #!/usr/bin/env python3
 
 from types import SimpleNamespace
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
 from rdkit import Chem
+import ase.io
+import networkx
+import networkx.algorithms.isomorphism as iso
+
+
+def get_atoms_in_order(smi):
+    mol = Chem.MolFromSmiles(smi, sanitize=False)
+    Chem.SanitizeMol(mol)
+    atoms   = np.array([at.GetSymbol() for at in mol.GetAtoms()])
+    mapping = np.array([at.GetAtomMapNum() for at in mol.GetAtoms()])
+    assert np.all(mapping > 0)
+    atoms, mapping = map(np.array, zip(*[(at.GetSymbol(), at.GetAtomMapNum()) for at in mol.GetAtoms()]))
+    return atoms[np.argsort(mapping)]
+
+
+def make_nx_graph(atoms, bonds):
+    G = networkx.Graph()
+    G.add_nodes_from([(i, {'q': q}) for i, q in enumerate(atoms)])
+    G.add_edges_from(bonds)
+    return G
+
+
+def make_xyz_graph(filename):
+
+    mol = ase.io.read(filename)
+    atoms = np.array(mol.get_chemical_symbols())
+
+    cutoff = [ase.data.covalent_radii[z] for z in mol.get_atomic_numbers()]
+    nl = ase.neighborlist.NeighborList(cutoff, self_interaction=False, bothways=False)
+    nl.update(mol)
+    AC = nl.get_connectivity_matrix(sparse=True)
+    bonds = np.array(sorted(sorted(i) for i in AC.keys()))
+
+    return make_nx_graph(atoms, bonds), atoms
+
+
+def make_rdkit_graph(smi):
+    mol = Chem.MolFromSmiles(smi)
+    mol = Chem.AddHs(mol)
+    atoms = np.array([at.GetSymbol() for at in mol.GetAtoms()])
+    bonds = np.array(sorted(sorted((i.GetBeginAtomIdx(), i.GetEndAtomIdx())) for i in mol.GetBonds()))
+    return make_nx_graph(atoms, bonds), atoms, mol
+
+
+def map_smiles(xyz, smi1, smi2, normal_Si_coordnum):
+
+    # Make graphs.
+    # Choose SMILES from normal (smi1) and alternative (smi2)
+    # based on Silicon coordination number
+    G2, xyz_atoms = make_xyz_graph(xyz)
+    Si_coordnum = len(G2.edges(np.where(xyz_atoms=='Si')[0][0]))
+    smi = smi1 if Si_coordnum==normal_Si_coordnum else smi2
+    G1, rdkit_atoms, mol = make_rdkit_graph(smi)
+
+    # Match graphs
+    GM = iso.GraphMatcher(G1, G2, node_match=iso.categorical_node_match('q', None))
+    assert GM.is_isomorphic(), f'not isomorfic {xyz}'
+    match = next(GM.match())
+    src, dst = np.array(sorted(match.items(), key=lambda match: match[0])).T
+    assert np.all(src==np.arange(G1.number_of_nodes())), xyz
+    assert np.all(rdkit_atoms==xyz_atoms[dst]), xyz
+
+    # Map atoms
+    for i, at in enumerate(mol.GetAtoms()):
+        at.SetAtomMapNum(int(dst[i])+1)
+
+    # Check mapping
+    maps = np.array([at.GetAtomMapNum() for at in mol.GetAtoms()])-1
+    assert np.all(rdkit_atoms==xyz_atoms[maps]), xyz
+
+    return Chem.MolToSmiles(mol)
 
 
 def clean_smiles(x):
@@ -103,19 +177,32 @@ def construct_reactants_products(ligands):
     return reactions
 
 
-def main(data_dir='benchmark-barrier-learning/data/proparg'):
+def main(data_dir='data/proparg'):
 
     ligands = construct_ligands()
     reactions = construct_reactants_products(ligands)
 
-    for key in ligands.keys():
-        print(key)
-        print(reactions[key].reactant)
-        print(reactions[key].reactant_1SiONbond)
-        print(reactions[key].product)
-        print(reactions[key].product_1SiONbond)
-        print()
+    df = pd.read_csv(f'{data_dir}/data.csv', index_col=0)
+    labels = df['mol'].values
+    enans  = df['enan'].values
 
+    for label, enan in tqdm(zip(labels, enans), total=len(df)):
+
+        xyz_reactant, xyz_product = [f'{data_dir}/data_{comp}_xyz/{label}{enan}.xyz' for comp in ('react', 'prod')]
+        smis = reactions[label[:-1]]
+
+        try:
+            reactant_mapped = map_smiles(xyz_reactant, smis.reactant, smis.reactant_1SiONbond, 6)
+            product_mapped  = map_smiles(xyz_product,  smis.product,  smis.product_1SiONbond,  5)
+            arm, apm = map(get_atoms_in_order, (reactant_mapped, product_mapped))
+            assert np.all(arm==apm)
+
+        except Exception as e:
+            print('\n', '\033[1;91m', e, '\033[0m')
+            continue
+
+        df.loc[(df['mol']==label) & (df['enan']==enan), 'rxn_smiles_mapped'] = reactant_mapped+'>>'+product_mapped
+    df.to_csv('proparg-mapped.csv')
 
 if __name__=='__main__':
     main()

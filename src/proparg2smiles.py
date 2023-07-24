6c822307b8958a2dffa123209f65e44c35ce17c3
Ksenia <ksenia.briling@epfl.ch>
2023-07-24 17:23:52 +0200

    Fix xyz2mol smiles

--- src/proparg2smiles.py
+++ src/proparg2smiles.py
@@ -7,6 +7,13 @@ from rdkit import Chem
 from smilify import smilify
 
 
+def clean_mapping(smi):
+    mol = Chem.MolFromSmiles(smi, sanitize=False)
+    for a in mol.GetAtoms():
+        a.SetAtomMapNum(0)
+    return Chem.MolToSmiles(mol)
+
+
 def get_atoms_in_order(smi):
     mol = Chem.MolFromSmiles(smi, sanitize=False)
     Chem.SanitizeMol(mol)
@@ -24,13 +31,19 @@ def main(data_dir='data/proparg'):
 
     for label, enan in tqdm(zip(labels, enans), total=len(df)):
         xyzs = [f'{data_dir}/data_{comp}_xyz/{label}{enan}.xyz' for comp in ('react', 'prod')]
-        smis = [smilify(xyz, mapping=True) for xyz in xyzs]
+        smis_mapped = [smilify(xyz, mapping=True) for xyz in xyzs]
         try:
-            arm, apm = map(get_atoms_in_order, smis)
+            arm, apm = map(get_atoms_in_order, smis_mapped)
             assert np.all(arm==apm)
-            df.loc[(df['mol']==label) & (df['enan']==enan), 'rxn_smiles_mapped'] = smis[0]+'>>'+smis[1]
+            rsmi_mapped = smis_mapped[0]+'>>'+smis_mapped[1]
+            smis = [clean_mapping(smi) for smi in smis_mapped]
+            rsmi = smis[0]+'>>'+smis[1]
         except:
             print('\n' + '\033[1;91m' + f'{label}{enan} is bad' + '\033[0m')
+            rsmi = ''
+            rsmi_mapped = ''
+        df.loc[(df['mol']==label) & (df['enan']==enan), 'rxn_smiles_mapped'] = rsmi_mapped
+        df.loc[(df['mol']==label) & (df['enan']==enan), 'rxn_smiles'] = rsmi
     df.to_csv('proparg-mapped.csv')
 
 

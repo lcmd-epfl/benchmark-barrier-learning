0070f76916550a699cca1fbc1764045286168ec4
Ksenia <ksenia.briling@epfl.ch>
2023-07-25 17:14:01 +0200

    Add gdb_mod and random map it

--- src/random_mapper.py
+++ src/random_mapper.py
@@ -10,6 +10,7 @@ def argparse():
     parser.add_argument('-g', '--gdb', action='store_true')
     parser.add_argument('-p', '--proparg', action='store_true')
     parser.add_argument('--proparg_arom', action='store_true')
+    parser.add_argument('--gdb_mod', action='store_true')
     parser.add_argument('--seed', type=int, default=2)
     args = parser.parse_args()
     return args
@@ -53,6 +54,7 @@ if __name__ == "__main__":
         (args.gdb,          "data/gdb7-22-ts/ccsdtf12_dz.csv",      'rxn_smiles',        'reactant'),
         (args.proparg,      "data/proparg/data.csv",                'rxn_smiles_mapped', 'product'),
         (args.proparg_arom, "data/proparg/data_fixarom_smiles.csv", 'rxn_smiles_mapped', 'product'),
+        (args.gdb_mod,      "data/gdb7-22-ts/ccsdtf12_dz_mod.csv",  'rxn_smiles',        'reactant'),
     )
 
     for flag, dfile, src_column, component in datasets:

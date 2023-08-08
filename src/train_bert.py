ebcce240c7689ef14ee488f4360610e860494a6a
Ksenia <ksenia.briling@epfl.ch>
2023-08-08 13:27:59 +0200

    Fix patch for bert
    The patch used affects the MFP and CGR results...

--- src/train_bert.py
+++ src/train_bert.py
@@ -38,6 +38,8 @@ def remove_atom_mapping(smi):
             print("could not convert smi", smi, "to mol")
             return smi
         Chem.SanitizeMol(mol, sanitizeOps=Chem.SanitizeFlags.SANITIZE_ALL ^ Chem.SanitizeFlags.SANITIZE_PROPERTIES)
+        mol = Chem.RemoveHs(mol, sanitize=False)
+        Chem.SanitizeMol(mol, sanitizeOps=Chem.SanitizeFlags.SANITIZE_ALL ^ Chem.SanitizeFlags.SANITIZE_PROPERTIES)
     else:
         mol = m
 
